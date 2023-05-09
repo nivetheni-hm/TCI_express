94b31b98ae146a1cafda07955c7077aeb15d552e
sachin <sachin@happymonk.co>
2023-05-09 16:29:45 +0530

    added anamoly classification

--- anamoly_track.py
+++ anamoly_track.py
@@ -157,6 +157,7 @@ async def process_publish(device_id,batch_data):
     # print(batch_data)
 
     output_json = output_func(batch_data)
+    
     # print(output_json)
     batchId = str(uuid.uuid4())
     output_json["batchid"] = batchId
@@ -169,11 +170,19 @@ async def process_publish(device_id,batch_data):
     tenant_id = "test_tenant_id"
     output_json["tenant_id"] = tenant_id
     output_json["metaData"]['detect'] = len(output_json["metaData"]['object'])
-    output_json["metaData"]['count'] = len(output_json["metaData"]['object'])
+    output_json["metaData"]['count']["peopleCount"] = len(output_json["metaData"]['object'])
     output_json["version"] = "v0.0.3"
-    print(output_json)
-    await json_publish_activity(primary=output_json)
+    anamoly = ["carrying","throwing","sitting on the box","standing on the box"]
+    if [True for each in [each["activity"][0] for each in output_json['metaData']['object']] if each in anamoly][0]:
+        output_json["type"] = "anamoly"
+        await json_publish_activity(primary=output_json)
 
+    print(output_json)
+    with open("test.json", "a") as outfile:
+        # data = json.load(outfile)
+        # data.append(output_json)
+        json.dump(output_json, outfile)
+    
 def trackmain(
     input,
     device_id ,
@@ -232,7 +241,6 @@ def trackmain(
             crop_img = save_one_box([*box], im, save=False)
             did = ""
             track_type = "100"
-            # print(len(confff),len(pred))
             cd = int(trackid)
             cidd = [crop_img]
             if len(confff) == len(pred[0]):
@@ -265,13 +273,14 @@ def trackmain(
             isolate_queue[device_id] = []
             isolate_queue[device_id].append(final_frame)
         
-        # print([{each:len(isolate_queue[each])} for each in isolate_queue])
+        print([{each:len(isolate_queue[each])} for each in isolate_queue])
         for each in isolate_queue:
             
             if len(isolate_queue[each])>29:
                 print("batch length of ",device_id,":",len(isolate_queue[each]))
                 batch_data = isolate_queue[each]
                 isolate_queue[each] = []
+                
                 asyncio.run(process_publish(device_id,batch_data))
 
 
